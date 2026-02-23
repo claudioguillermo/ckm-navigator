37eb4fefa6880a5bb3d9eb7c73c1adc4a1901cb2
Thomas Tsai <tomscy2000@Thomass-MacBook-Pro.local>
2026-02-23 17:31:16 -0500

    Phase 1: Fix secure storage and blank modules

--- test_regex.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const source = fs.readFileSync('main.js', 'utf8');
-const malformedTagRegex = /<\s+(div|h[1-6]|strong)\b/;
-const malformedDataAttrRegex = /data\s*-\s*args/;
-console.log('Tag match:', malformedTagRegex.exec(source));
-console.log('Attr match:', malformedDataAttrRegex.exec(source));
